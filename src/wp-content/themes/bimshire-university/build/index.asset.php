2d0204cb364a5a59483affda7baf8f7bece6f584
Janus Developer <dev@retrievo.net>
2024-07-07 22:51:19 -0400

    refactor search UI to use plain JS instead of JQuery

--- src/wp-content/themes/bimshire-university/build/index.asset.php
+++ src/wp-content/themes/bimshire-university/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'ac6cff961437fa6eb3b7');
+<?php return array('dependencies' => array(), 'version' => 'ca9a88b83a57e54f97cf');
--- src/wp-content/themes/bimshire-university/src/modules/search.js
+++ src/wp-content/themes/bimshire-university/src/modules/search.js
@@ -1,13 +1,13 @@
-import $ from 'jquery';
+import axios from 'axios';
 
 class Search {
     constructor() {
         this.addSearchUI();
-        this.searchOverlay = $('.search-overlay');
-        this.openButton = $('.js-search-trigger');
-        this.closeButton = $('.search-overlay__close');
-        this.searchField = $('#search-term');
-        this.searchResults = $('.search-overlay__results');
+        this.searchOverlay = document.querySelector('.search-overlay');
+        this.openButton = document.querySelectorAll('.js-search-trigger');
+        this.closeButton = document.querySelector('.search-overlay__close');
+        this.searchField = document.querySelector('#search-term');
+        this.searchResults = document.querySelector('#search-overlay__results');
 
         this.isOverLayOpen = false;
         this.isSpinnerVisible = false;
@@ -18,23 +18,30 @@ class Search {
     }
 
     events() {
-        this.openButton.on('click', this.openOverlay.bind(this));
-        this.closeButton.on('click', this.closeOverlay.bind(this));
-        $(document).on('keydown', this.keyPressDispatcher.bind(this));
-        this.searchField.on('keyup', this.processSearchTerm.bind(this));
+        this.openButton.forEach((el) => {
+            el.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.openOverlay();
+            });
+        });
+        this.closeButton.addEventListener('click', (e) => this.closeOverlay());
+        document.addEventListener('keydown', (e) => this.keyPressDispatcher(e));
+        this.searchField.addEventListener('keyup', (e) =>
+            this.processSearchTerm()
+        );
     }
 
     openOverlay() {
-        this.searchField.val('');
-        this.searchResults.html('');
-        this.searchOverlay.addClass('search-overlay--active');
-        $('body').addClass('body-no-scroll');
+        this.searchField.value = '';
+        this.searchResults.innerHTML = '';
+        this.searchOverlay.classList.add('search-overlay--active');
+        document.body.classList.add('body-no-scroll');
         this.isOverLayOpen = true;
     }
 
     closeOverlay() {
-        this.searchOverlay.removeClass('search-overlay--active');
-        $('body').removeClass('body-no-scroll');
+        this.searchOverlay.classList.remove('search-overlay--active');
+        document.body.classList.remove('body-no-scroll');
         this.isOverLayOpen = false;
     }
 
@@ -43,145 +50,145 @@ class Search {
     }
 
     processSearchTerm(e) {
-        if (this.searchField.val() !== this.previousSearchText) {
+        if (this.searchField.value !== this.previousSearchText) {
             clearTimeout(this.debounce);
 
-            if (this.searchField.val()) {
+            if (this.searchField.value) {
                 if (!this.isSpinnerVisible) {
-                    this.searchResults.html(
-                        '<div class="spinner-loader"></div>'
-                    );
+                    this.searchResults.innerHTML =
+                        '<div class="spinner-loader"></div>';
                     this.isSpinnerVisible = true;
                 }
-                this.debounce = setTimeout(this.getResults.bind(this), 500);
+                this.debounce = setTimeout(() => this.getResults(), 500);
             } else {
-                this.searchResults.html('');
+                this.searchResults.innerHTML = '';
                 this.isSpinnerVisible = false;
             }
         }
 
-        this.previousSearchText = this.searchField.val();
+        this.previousSearchText = this.searchField.value;
     }
 
-    getResults() {
+    async getResults() {
         const apiUrl = `${universityData.baseUrl}/wp-json/university/v1`;
 
-        if (this.searchField.val().trim().length > 3) {
-            $.getJSON(
-                `${apiUrl}/search?term=${this.searchField.val()}`,
-                (results) => {
-                    this.searchResults.html(`
-                    <div class="row">
-                        <div class="one-third">
-                            <h2 class="search-overlay__section-title">General Information</h2>
+        if (this.searchField.value.trim().length > 3) {
+            const response = await axios.get(
+                `${apiUrl}/search?term=${this.searchField.value}`
+            );
+            const results = response.data;
+
+            this.searchResults.innerHTML = `
+                <div class="row">
+                    <div class="one-third">
+                        <h2 class="search-overlay__section-title">General Information</h2>
+                        ${
+                            results['posts'].length
+                                ? '<ul class="link-list min-list">'
+                                : '<p>No results found</p>'
+                        }
+                        ${results['posts']
+                            .map(
+                                (result) =>
+                                    `<li><a href="${result.permalink}">${
+                                        result.title
+                                    }</a> ${
+                                        result.postType == 'post'
+                                            ? ` by ${result.authorName}`
+                                            : ''
+                                    }</li>`
+                            )
+                            .join('')}
+                        ${results['posts'].length ? '</ul>' : ''}
+                    </div>
+                    <div class="one-third">
+                        <h2 class="search-overlay__section-title">Programs</h2>
                             ${
-                                results['posts'].length
+                                results['programs'].length
                                     ? '<ul class="link-list min-list">'
                                     : '<p>No results found</p>'
                             }
-                            ${results['posts']
+                            ${results['programs']
                                 .map(
                                     (result) =>
-                                        `<li><a href="${result.permalink}">${
-                                            result.title
-                                        }</a> ${
-                                            result.postType == 'post'
-                                                ? ` by ${result.authorName}`
-                                                : ''
-                                        }</li>`
+                                        `<li><a href="${result.permalink}">${result.title}</a></li>`
                                 )
                                 .join('')}
-                            ${results['posts'].length ? '</ul>' : ''}
-                        </div>
-                        <div class="one-third">
-                            <h2 class="search-overlay__section-title">Programs</h2>
-                                ${
-                                    results['programs'].length
-                                        ? '<ul class="link-list min-list">'
-                                        : '<p>No results found</p>'
-                                }
-                                ${results['programs']
-                                    .map(
-                                        (result) =>
-                                            `<li><a href="${result.permalink}">${result.title}</a></li>`
-                                    )
-                                    .join('')}
-                                ${
-                                    results['programs'].length ? '</ul>' : ''
-                                }                            
-                            <h2 class="search-overlay__section-title">Professors</h2>
-                                ${
-                                    results['professors'].length
-                                        ? '<ul class="professor-cards">'
-                                        : '<p>No results found</p>'
-                                }
-                                ${results['professors']
-                                    .map(
-                                        (result) =>
-                                            `<li class="professor-card__list-item">
-                                                <a class="professor-card" href="${result.permalink}">
-                                                    <img class="professor-card__image" src="${result.image}" />
-                                                    <span class="professor-card__name">${result.title}</span>
-                                                </a>
-                                            </li>`
-                                    )
-                                    .join('')}
-                                ${
-                                    results['professors'].length ? '</ul>' : ''
-                                }                                
-                        </div>
-                        <div class="one-third">
-                            <h2 class="search-overlay__section-title">Campuses</h2>
-                                ${
-                                    results['campuses'].length
-                                        ? '<ul class="link-list min-list">'
-                                        : '<p>No results found</p>'
-                                }
-                                ${results['campuses']
-                                    .map(
-                                        (result) =>
-                                            `<li><a href="${result.permalink}">${result.title}</a></li>`
-                                    )
-                                    .join('')}
-                                ${
-                                    results['campuses'].length ? '</ul>' : ''
-                                }                                                        
-                            <h2 class="search-overlay__section-title">Events</h2>
-                                ${
-                                    results['events'].length
-                                        ? ''
-                                        : '<p>No results found</p>'
-                                }
-                                ${results['events']
-                                    .map(
-                                        (result) =>
-                                            `<div class="event-summary">
-                                                    <a class="event-summary__date t-center" href="${result.permalink}">
-                                                        <span class="event-summary__month">${result.month}</span>
-                                                        <span class="event-summary__day">${result.day}</span>
-                                                    </a>
-                                                    <div class="event-summary__content">
-                                                        <h5 class="event-summary__title headline headline--tiny"><a href="${result.permalink}">${result.title}</a></h5>
-                                                        <p>
-                                                            ${result.summary}&nbsp;<a href="${result.permalink}" class="nu gray">Learn more</a>
-                                                        </p>
-                                                    </div>
-                                                </div>`
-                                    )
-                                    .join('')}                             
-                        </div>
+                            ${
+                                results['programs'].length ? '</ul>' : ''
+                            }                            
+                        <h2 class="search-overlay__section-title">Professors</h2>
+                            ${
+                                results['professors'].length
+                                    ? '<ul class="professor-cards">'
+                                    : '<p>No results found</p>'
+                            }
+                            ${results['professors']
+                                .map(
+                                    (result) =>
+                                        `<li class="professor-card__list-item">
+                                            <a class="professor-card" href="${result.permalink}">
+                                                <img class="professor-card__image" src="${result.image}" />
+                                                <span class="professor-card__name">${result.title}</span>
+                                            </a>
+                                        </li>`
+                                )
+                                .join('')}
+                            ${
+                                results['professors'].length ? '</ul>' : ''
+                            }                                
                     </div>
-                `);
-                }
-            );
+                    <div class="one-third">
+                        <h2 class="search-overlay__section-title">Campuses</h2>
+                            ${
+                                results['campuses'].length
+                                    ? '<ul class="link-list min-list">'
+                                    : '<p>No results found</p>'
+                            }
+                            ${results['campuses']
+                                .map(
+                                    (result) =>
+                                        `<li><a href="${result.permalink}">${result.title}</a></li>`
+                                )
+                                .join('')}
+                            ${
+                                results['campuses'].length ? '</ul>' : ''
+                            }                                                        
+                        <h2 class="search-overlay__section-title">Events</h2>
+                            ${
+                                results['events'].length
+                                    ? ''
+                                    : '<p>No results found</p>'
+                            }
+                            ${results['events']
+                                .map(
+                                    (result) =>
+                                        `<div class="event-summary">
+                                                <a class="event-summary__date t-center" href="${result.permalink}">
+                                                    <span class="event-summary__month">${result.month}</span>
+                                                    <span class="event-summary__day">${result.day}</span>
+                                                </a>
+                                                <div class="event-summary__content">
+                                                    <h5 class="event-summary__title headline headline--tiny"><a href="${result.permalink}">${result.title}</a></h5>
+                                                    <p>
+                                                        ${result.summary}&nbsp;<a href="${result.permalink}" class="nu gray">Learn more</a>
+                                                    </p>
+                                                </div>
+                                            </div>`
+                                )
+                                .join('')}                             
+                    </div>
+                </div>
+            `;
         }
 
         this.isSpinnerVisible = false;
     }
 
     addSearchUI() {
-        $('body').append(`
-            <div class="search-overlay">
+        document.body.insertAdjacentHTML(
+            'beforeend',
+            `<div class="search-overlay">
                 <div class="search-overlay__top">
                     <div class="container">
                         <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
@@ -190,10 +197,10 @@ class Search {
                     <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
                 </div>
                 <div class="container">
-                    <div class="search-overlay__results"></div>
+                    <div id="search-overlay__results"></div>
                 </div>
-            </div>            
-        `);
+            </div>`
+        );
     }
 }
 
